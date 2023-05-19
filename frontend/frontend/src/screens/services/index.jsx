6fd2bf48b8d52911f094f4c824e7e50bcb3ef401
Jacek Pilch <jaaahooou@wp.pl>
2023-05-19 21:29:12 +0200

    Add protection againt unauthorized access

--- frontend/frontend/src/screens/services/index.jsx
+++ frontend/frontend/src/screens/services/index.jsx
@@ -1,27 +1,48 @@
-import React from 'react'
-import NavbarTop from '../../components/NavbarTop'
-
+import React from "react";
+import NavbarTop from "../../components/NavbarTop";
+import { useSelector } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Services = () => {
-  return (
-   <main className='bg-secondary-bg-color h-screen'>
-    <NavbarTop/>
-    <div className=" mx-6 my-8">
-    <div className="text-xl text-center text-white bg-[#629D93] p-1" >
-    <h1>Chose service</h1>
-    </div>
-    <div className="grid grid-rows-layout grid-cols-2 gap-1">
-    <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">Table Service</div>
-    <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">Pending orders</div>
-    <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">Quick Order</div>
-    <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">Delivery</div>
-    {/* <div className="p-8 bg-white">Pending Orders</div>
-    <div className="p-8 bg-white">Quick Orders</div>
-    <div className="p-8 bg-white">Delivery</div> */}
-    </div>
-    </div>
-   </main>
-  )
-}
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
 
-export default Services
+  console.log(userInfo);
+  return loading ? (
+    <CircularProgress color="secondary" />
+  ) : error ? (
+    <div>Something went wrong</div>
+  ) : (
+    <main className="bg-secondary-bg-color h-screen">
+      <NavbarTop />
+      {userInfo ? (
+        <div className=" mx-6 my-8">
+          <div className="text-xl text-center text-white bg-[#629D93] p-1">
+            <h1>Chose service</h1>
+          </div>
+          <div className="grid grid-rows-layout grid-cols-2 gap-1">
+            <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">
+              Table Service
+            </div>
+            <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">
+              Pending orders
+            </div>
+            <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">
+              Quick Order
+            </div>
+            <div className="flex justify-center items-center  bg-white text-center font-bold text-xl">
+              Delivery
+            </div>
+            {/* <div className="p-8 bg-white">Pending Orders</div>
+        <div className="p-8 bg-white">Quick Orders</div>
+        <div className="p-8 bg-white">Delivery</div> */}
+          </div>
+        </div>
+      ) : (
+        <div>You must be logged in</div>
+      )}
+    </main>
+  );
+};
+
+export default Services;
