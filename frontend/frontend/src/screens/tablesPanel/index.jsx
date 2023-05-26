450f707cc22db87f11f471ba6db6861747b20191
molszewski34 <m.olszewski341@gmail.com>
2023-05-26 12:10:07 +0200

    added: Submenu to NavbarTop, minor changes in styling

--- frontend/frontend/src/screens/tablesPanel/index.jsx
+++ frontend/frontend/src/screens/tablesPanel/index.jsx
@@ -16,8 +16,8 @@ const TablesPanel = () => {
   const orderList = useSelector((state) => state.orderList);
   const { error, loading, orders } = orderList;
 
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  // const theme = useTheme();
+  // const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
 
   const tableList = useSelector((state) => state.tableList);
@@ -60,18 +60,20 @@ const TablesPanel = () => {
     <main className="bg-secondary-bg-color h-screen relative flex flex-col  ">
       <NavbarTop />
       <h1 className="font-bold text-[#0f766e] text-3xl text-center pl-2 my-4 ml-2">
-        Tables
+        Rooms
       </h1>
 
-      {/* <div className="flex justify-center mb-4"> */}
+ 
+   
       <div className="grid grid-cols-4 grid-flow-row auto-rows-fr gap-y-4 mb-4 md:min-w-[800px] md:max-w-[50vw] md:place-self-center">
         {rooms.map((room, index) => (
    
           <button
             key={room.id}
             className={`p-2 mx-2 text-base text-white bg-primary-bg-color shadow-xl  ${
-              activeTab === index ? "text-[#fde047] font-bold border-2 border-white"  : "text-[#374151] border-2 border-primary-bg-color"
+              activeTab === index ? "text-[#f1fd47] font-bold border-2 border-white"  : "text-[#374151] border-2 border-primary-bg-color"
             }`}
+               /* Renders rooms acording to active tab state and index, also adds styling to active tab button*/
             onClick={() => handleTabChange(index)}
           >
             {room.name}
@@ -85,16 +87,23 @@ const TablesPanel = () => {
           key={room.id}
           className={`p-2 md:p-4 md:min-w-[800px] md:max-w-[50vw] md:place-self-center md:border-2 md:border-[#337066] md:mb-6 md:rounded md:shadow-md ${
             activeTab !== index ? "hidden" : ""
-          }`}
+          }`}  
+        // if activeTab is no selected - activeTab is hidden
         >
-          <header className="flex items-center text-lg text-[#374151] mb-2">
+          <header className="flex items-center text-lg font-bold text-[#374151] mb-2">
 
-            <MdTableBar className="text-lg text-white bg-[#ea580c] rounded-full p-1 w-6 h-6 mr-2 border border-white shadow"
+            <MdTableBar className="text-lg text-[#f1fd47] bg-primary-bg-color rounded-full p-1 w-6 h-6 mr-2 shadow"
           />
           {room.name}
         </header>
         <div className="grid grid-cols-4 place-content-center gap-1 gap-y-3 mb-5 bg-secondary-bg-color grid-flow-row">
-          {/* DISPLAY TABLES */}
+     
+        {/* TABLES RENDERING
+        1. Object tables is .filter by table.room 
+        2. Acquired array is .map to filteredTable
+        3. If filteredTable isOccupied filteredTable is has new styling sugesting his state: border: "2px dashed red"
+        */}
+        
           {tables
             .filter((table) => table.room === room.id)
             .map((filteredTable) => (
@@ -115,7 +124,7 @@ const TablesPanel = () => {
                         >
                           <button
                             key={filteredTable.id}
-                            disabled
+                            // disabled
                             className="flex flex-col items-center justify-center duration-200 font-bold border-2 border-primary-bg-color rounded shadow bg-white opacity-50 cursor-not-allowed"
                           >
                             <span className="text-3xl">
@@ -127,8 +136,10 @@ const TablesPanel = () => {
                       ))}
                   </>
                 ) : (
-                  // DISPLAY FREE TABLES
-                  <button
+                  /* PASSING PROPS TO MODAL
+                  1. onClick passing param setSelectedMaxNumOfGuests(filteredTable.numberOfPersons) to ModalTablesPanel*/
+                  
+                   <button
                     key={filteredTable.id}
                     className="flex flex-col items-center justify-center bg-[#f0fdfa] duration-200 font-bold border-2 hover:border-dotted border-primary-bg-color rounded shadow"
                     onClick={() => {
