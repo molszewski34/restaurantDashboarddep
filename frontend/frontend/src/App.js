2094815ad6cf6b3b4d018cd643c45e2bc6f99707
Jacek Pilch <jaaahooou@wp.pl>
2023-06-15 19:55:19 +0200

    update

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import { Routes, Route, HashRouter as Router } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, HashRouter as Router } from "react-router-dom";
 
-import { createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Dashboard from './screens/dashboard';
-import DishMenu from './screens/DishMenu';
+import { createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Dashboard from "./screens/dashboard";
+import DishMenu from "./screens/DishMenu";
 
-import Orders from './screens/orders/index';
-import Admin from './screens/admin';
-import TablesMap from './screens/tablesMap';
-import Staff from './screens/staff';
-import Login from './screens/login';
-import Services from './screens/services';
-import StartingPanel from './screens/startingPanel';
-import Invite from './screens/invite';
-import TablesPanel from './screens/tablesPanel';
-import OrdersPanel from './screens/ordersPanel';
-import Labor from './screens/manager_activities/labor/editLabor';
-import Dishes from './screens/manager_activities/dishes';
-import CategoriesList from './screens/manager_activities/dishes/categoriesList';
-import NewCategory from './screens/manager_activities/dishes/newCategory';
-import EditCategory from './screens/manager_activities/dishes/editCategory';
-import NewDish from './screens/manager_activities/dishes/newDish';
-import NewRoom from './screens/manager_activities/tables/addRoom';
-import EditRoom from './screens/manager_activities/tables/editRoom';
-import AddTable from './screens/manager_activities/tables/addTable';
-import TablesList from './screens/manager_activities/tables/tablesList';
-import LaborsList from './screens/manager_activities/labor/laborsList';
-import NewLabor from './screens/manager_activities/labor/newLabor';
+import Orders from "./screens/orders/index";
+import Admin from "./screens/admin";
+import TablesMap from "./screens/tablesMap";
+import Staff from "./screens/staff";
+import Login from "./screens/login";
+import Services from "./screens/services";
+import StartingPanel from "./screens/startingPanel";
+import Invite from "./screens/invite";
+import TablesPanel from "./screens/tablesPanel";
+import OrdersPanel from "./screens/ordersPanel";
+import Labor from "./screens/manager_activities/labor";
+import Dishes from "./screens/manager_activities/dishes";
+import CategoriesList from "./screens/manager_activities/dishes/categoriesList";
+import NewCategory from "./screens/manager_activities/dishes/newCategory";
+import EditCategory from "./screens/manager_activities/dishes/editCategory";
+import NewDish from "./screens/manager_activities/dishes/newDish";
+import NewRoom from "./screens/manager_activities/tables/addRoom";
+import EditRoom from "./screens/manager_activities/tables/editRoom";
+import AddTable from "./screens/manager_activities/tables/addTable";
+import TablesList from "./screens/manager_activities/tables/tablesList";
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
@@ -58,12 +56,14 @@ function App() {
               <Route path="/labor" element={<Labor />} />
               {/* <Route path="/menu" element={<Dishes />} /> */}
               <Route path="/menu" element={<CategoriesList />} />
+              {/* 
               <Route path="/newCategory" element={<NewCategory />} />
               <Route path="/editCategory" element={<EditCategory />} />
               <Route path="/newDish" element={<NewDish />} />
               <Route path="/newRoom" element={<NewRoom />} />
               <Route path="/editRoom" element={<EditRoom />} />
-              <Route path="/addTable" element={<AddTable />} />
+              <Route path="/addTable" element={<AddTable />} /> */}
+
               <Route path="/tablesList" element={<TablesList />} />
               <Route path="/laborsList" element={<LaborsList />} />
               <Route path="/newLabor" element={<NewLabor />} />
