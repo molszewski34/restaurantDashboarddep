9d943810bee5d65ba685af6bcea5f71f147fd31e
Jacek Pilch <jaaahooou@wp.pl>
2023-06-15 20:14:19 +0200

    cleaning app.js

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -17,20 +17,9 @@ import Invite from "./screens/invite";
 import TablesPanel from "./screens/tablesPanel";
 import OrdersPanel from "./screens/ordersPanel";
 import Labor from "./screens/manager_activities/labor";
-import Dishes from "./screens/manager_activities/dishes";
 import CategoriesList from "./screens/manager_activities/dishes/categoriesList";
-import NewCategory from "./screens/manager_activities/dishes/newCategory";
-import EditCategory from "./screens/manager_activities/dishes/editCategory";
-import NewDish from "./screens/manager_activities/dishes/newDish";
-import NewRoom from "./screens/manager_activities/tables/addRoom";
 import EditRoom from "./screens/manager_activities/tables/editRoom";
-import AddTable from "./screens/manager_activities/tables/addTable";
 import TablesList from "./screens/manager_activities/tables/tablesList";
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
 
 function App() {
   return (
@@ -54,19 +43,8 @@ function App() {
               <Route path="/services" element={<Services />} />
               <Route path="/tablesPanel" element={<TablesPanel />} />
               <Route path="/labor" element={<Labor />} />
-              {/* <Route path="/menu" element={<Dishes />} /> */}
               <Route path="/menu" element={<CategoriesList />} />
-              {/* 
-              <Route path="/newCategory" element={<NewCategory />} />
-              <Route path="/editCategory" element={<EditCategory />} />
-              <Route path="/newDish" element={<NewDish />} />
-              <Route path="/newRoom" element={<NewRoom />} />
-              <Route path="/editRoom" element={<EditRoom />} />
-              <Route path="/addTable" element={<AddTable />} /> */}
-
               <Route path="/tablesList" element={<TablesList />} />
-              <Route path="/laborsList" element={<LaborsList />} />
-              <Route path="/newLabor" element={<NewLabor />} />
               <Route path="/tablesList/:roomId" element={<EditRoom />} />
             </Routes>
           </main>
