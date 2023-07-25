392f21357c597439da6a8ab877a269b8efba0367
Jacek Pilch <jaaahooou@wp.pl>
2023-07-25 21:22:44 +0200

    optimalization of categoriesActions.js

--- frontend/frontend/src/actions/categoriesActions.js
+++ frontend/frontend/src/actions/categoriesActions.js
@@ -6,18 +6,28 @@ import {
 
 import axios from "axios";
 
-// ==============  LIST CATEGORIES ====================
+const handleRequestError = (error) => {
+  if (error.response?.status === 403) {
+    alert("You don't have permission to do that");
+  } else {
+    alert("Something went wrong, status code: " + error.response?.status);
+  }
+};
+
+const getHeadersConfig = () => {
+  let userInfo = JSON.parse(localStorage.userInfo);
+  return {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + String(userInfo.access),
+    },
+  };
+};
+
 export const listCategories = () => async (dispatch) => {
   try {
     dispatch({ type: CATEGORIES_LIST_REQUEST });
-    // =============== JWT AUTHORIZATION ==============================
-    let userInfo = JSON.parse(localStorage.userInfo);
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer " + String(userInfo.access),
-      },
-    };
+    const config = getHeadersConfig();
     const { data } = await axios.get("dishes/get-categories", config);
     dispatch({
       type: CATEGORIES_LIST_SUCCESS,
@@ -34,74 +44,36 @@ export const listCategories = () => async (dispatch) => {
   }
 };
 
-// ==============  LIST CATEGORIES === END ====================
-
-// ==============  CREATE NEW CATEGORY IN RESTAURANT ====================
-
 export const createNewCategory = (categoryName, colour) => async (dispatch) => {
   try {
-    let userInfo = JSON.parse(localStorage.userInfo);
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer " + String(userInfo.access),
-      },
-    };
-    // DATA SENT TO BACKEND
+    const config = getHeadersConfig();
     const body = {
       title: categoryName,
       colour: colour,
     };
 
-    const data = await axios
-      .post("dishes/create-category", body, config)
-      .then(function (response) {
-        if (response.status == 200) {
-          //if response is 200, list categories again.
-          alert("Create category status: OK");
-          dispatch(listCategories());
-        }
-        if (response.status == 403) {
-          alert("You don`t have permission to do that");
-        } else {
-          alert("Something went wrong, status code: ", response.status);
-        }
-      });
+    const response = await axios.post("dishes/create-category", body, config);
+    if (response.status === 200) {
+      alert("Create category status: OK");
+      dispatch(listCategories());
+    }
   } catch (error) {
-    alert(error);
+    handleRequestError(error);
   }
 };
-
-// ==============  CREATE NEW CATEGORY IN RESTAURANT === END ====================
-
-// ==============  REMOVE CATEGORY FROM RESTAURANT ====================
 
 export const removeCategory = (categoryId) => async (dispatch) => {
   try {
-    let userInfo = JSON.parse(localStorage.userInfo);
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer " + String(userInfo.access),
-      },
-    };
-
-    const data = await axios
-      .delete(`dishes/delete-category/${categoryId}`, config)
-      .then(function (response) {
-        if (response.status == 200) {
-          //if response is 200, alert 'status ok', dispatch - listCategories
-          alert("Remove category status: OK");
-          dispatch(listCategories());
-        }
-      });
-  } catch (error) {
-    if (error.response.status == 403) {
-      alert("You don`t have permission to do that");
-    } else {
-      alert(error);
+    const config = getHeadersConfig();
+    const response = await axios.delete(
+      `dishes/delete-category/${categoryId}`,
+      config
+    );
+    if (response.status === 200) {
+      alert("Remove category status: OK");
+      dispatch(listCategories());
     }
+  } catch (error) {
+    handleRequestError(error);
   }
 };
-
-// ==============  REMOVE CATEGORY FROM RESTAURANT === END ====================
