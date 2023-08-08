5096db84bdbe2561db65b5677e7b1876201be318
molszewski34 <m.olszewski341@gmail.com>
2023-08-08 08:23:02 +0200

    Service panel styling

--- frontend/frontend/src/screens/services/index.jsx
+++ frontend/frontend/src/screens/services/index.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import NavbarTop from "../../components/navbars/NavbarTop";
-import { useSelector, useDispatch } from "react-redux";
-import CircularProgress from "@mui/material/CircularProgress";
-import { useNavigate } from "react-router-dom";
-import { listRooms } from "../../actions/tablesActions";
-import { listTables } from "../../actions/tablesActions";
-import { listActiveOrderDishes } from "../../actions/dishActions";
-import { listOrders } from "../../actions/ordersActions";
+import React from 'react';
+import NavbarTop from '../../components/navbars/NavbarTop';
+import { useSelector, useDispatch } from 'react-redux';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useNavigate } from 'react-router-dom';
+import { listRooms } from '../../actions/tablesActions';
+import { listTables } from '../../actions/tablesActions';
+import { listActiveOrderDishes } from '../../actions/dishActions';
+import { listOrders } from '../../actions/ordersActions';
+import { MdOutlineTableBar, MdTableBar, MdFastfood } from 'react-icons/md';
+import { GiCook } from 'react-icons/gi';
 
+import { GoListOrdered } from 'react-icons/go';
 const Services = () => {
   let navigate = useNavigate();
   const dispatch = useDispatch();
@@ -23,65 +26,66 @@ const Services = () => {
     <main className=" flex flex-col items-center bg-secondary-bg-color h-screen">
       <NavbarTop />
       {userInfo.id ? (
-        <div className="">
-          <div className=" mx-6 my-8 grid max-w-[800px]">
+        <div className="text-[#6b7280]">
+          <div className=" mx-6 my-8 grid max-w-[800px] gap-2">
             <div className="text-xl text-center text-white bg-[#629D93] p-1">
               <h1>Mode</h1>
             </div>
-            <div className="grid grid-cols-2  gap-1">
+            <div className="grid grid-cols-2  gap-2">
               <button
                 onClick={() => {
                   dispatch(listTables());
                   dispatch(listRooms());
                   dispatch(listOrders());
-                  navigate("/tablesPanel");
+                  navigate('/tablesPanel');
                 }}
-                className="flex p-8 justify-center items-center  bg-white text-center font-bold text-xl"
+                className="flex flex-col p-8 justify-center items-center  bg-white text-center rounded-xl shadow"
               >
-                Table Service
+                <MdOutlineTableBar className="text-6xl font-bold" /> Table
+                Service
               </button>
               <button
                 onClick={() => {
                   dispatch(listActiveOrderDishes());
-                  navigate("/pending-orders");
+                  navigate('/pending-orders');
                 }}
-                className="flex justify-center items-center  bg-white text-center font-bold text-xl"
+                className="flex flex-col p-8 justify-center items-center  bg-white text-center  rounded shadow"
               >
-                Pending orders
+                <GoListOrdered className="text-6xl font-bold" /> Pending orders
               </button>
             </div>
           </div>
-          <div className=" mx-6 my-8 grid max-w-[800px]">
+          <div className=" mx-6 my-8 grid max-w-[800px] gap-2">
             <div className="text-xl text-center text-white bg-[#629D93] p-1">
               <h1>Manager Activities</h1>
             </div>
-            <div className="grid grid-cols-2 grid-rows-2 gap-1">
+            <div className="grid grid-cols-2 grid-rows-2 gap-2">
               <button
-                className="flex p-8 justify-center items-center  bg-white text-center font-bold text-xl"
+                className="flex flex-col p-8 justify-center items-center  bg-white text-center  rounded shadow"
                 onClick={() => {
-                  navigate("/menu");
+                  navigate('/menu');
                 }}
               >
-                Menu Managment
+                <MdFastfood className="text-6xl font-bold" /> Menu Managment
               </button>
               <button
                 onClick={() => {
-                  navigate("/employess");
+                  navigate('/employess');
                 }}
-                className="flex justify-center items-center  bg-white text-center font-bold text-xl"
+                className="flex flex-col p-8 justify-center items-center  bg-white text-center  rounded shadow"
               >
-                Employees Managment
+                <GiCook className="text-6xl font-bold" /> Employees Managment
               </button>
 
               <button
-                className="flex justify-center items-center  bg-white text-center font-bold text-xl"
+                className="flex flex-col p-8 justify-center items-center  bg-white text-center  rounded shadow"
                 onClick={() => {
-                  navigate("/tablesList");
+                  navigate('/tablesList');
                   dispatch(listRooms());
                   dispatch(listTables());
                 }}
               >
-                Rooms Managment
+                <MdTableBar className="text-6xl font-bold" /> Rooms Managment
               </button>
             </div>
           </div>
